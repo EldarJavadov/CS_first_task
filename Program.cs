2a74afcb7658a9f176028d034a7564fec60b3b0c
= <=>
2023-09-18 13:00:01 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number1;
+        int number2;
+        string symbol;
+
+
+        Console.WriteLine("Ilk reqemi daxil edin");
+        number1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Simvolu daxil edin(-+/*)");
+        symbol = Console.ReadLine();
+
+        Console.WriteLine("ikinci reqemi daxil edin");
+        number2 = int.Parse(Console.ReadLine());
+
+
+        switch (symbol)
+        {
+            case "+":
+            Console.WriteLine($"Berbardir={number1+number2}");
+            break;
+
+            case "-":
+            Console.WriteLine($"Berbardir={number1 - number2}");
+            break;
+            
+            case "*":
+            Console.WriteLine($"Berbardir={number1 *number2}");
+            break;
+
+            case "/":
+            Console.WriteLine($"Berbardir={number1 /number2}");
+            break;
+
+            default:
+            Console.WriteLine("Dogru simvol daxil edilmeyib");
+            break;
+        }
+
+    }
+}
